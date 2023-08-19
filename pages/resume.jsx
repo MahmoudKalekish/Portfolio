996957b0f2a8b1e7803f81ae47d605dd6194c4cb
Mahmoud Kalekish <mahmoudkalekish18@gmail.com>
2023-08-19 21:02:11 +0300

    editing name on mobile

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -18,7 +18,7 @@ const resume = () => {
       <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
         <h2 className='text-center'>Resume</h2>
         <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
-          <h2 className='text-center'>Mahmoud Kalekish</h2>
+          <h2 className='text-center name'>Mahmoud Kalekish</h2>
 
           <div className='flex'>
 
