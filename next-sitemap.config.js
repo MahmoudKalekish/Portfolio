17ccd3a36c6b139734d54e710fb92184d1594be9
Mahmoud Kalekish <mahmoudkalekish18@gmail.com>
2025-04-01 18:14:55 +0300

    SEO Site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
+// next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://mahmoud-kalekish.vercel.app',  // 👈 Your base website URL
-    generateRobotsTxt: true,                         // 👈 Automatically creates robots.txt
-    changefreq: 'weekly',                            // 👈 Tells Google how often your content changes
-    priority: 0.7,                                   // 👈 Tells Google how important your pages are (0.0 - 1.0)
+    siteUrl: 'https://mahmoud-kalekish.vercel.app',
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
   };
   
